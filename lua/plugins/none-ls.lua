38e274a37d8ae7168098e64cbed1532b36bda130
rafaratier <rafaelratier@hotmail.com>
2024-09-01 23:42:48 -0300

    fix: add dependencies to none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim"
+  },
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -8,8 +11,8 @@ return {
 				null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettier
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d")
 			},
 		})
 
